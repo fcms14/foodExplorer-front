4ef9156d7ff1743cd0aa355b76587bb283d31485
fcms14 <felipecms14@hotmail.com>
2022-08-09 16:45:54 -0300

    remoteUrl

--- src/services/api.js
+++ src/services/api.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const api = axios.create({
     // baseURL: "http://localhost:3333"
-    baseURL: "https://rocketfoodapi.herokuapp.com/"
+    baseURL: "https://rocketfood-api.herokuapp.com"
 });
