ed6bca5deaba09a0f1384078e2f724fe67d0d456
fcms14 <101072783+fcms14@users.noreply.github.com>
2023-03-08 23:25:35 -0300

    test

--- src/services/api.js
+++ src/services/api.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const api = axios.create({
     // baseURL: "http://localhost:3333"
-    baseURL: "https://rocketfood-api.herokuapp.com"
+    baseURL: "https://foodexplorer-api-production.up.railway.app/"
 });
